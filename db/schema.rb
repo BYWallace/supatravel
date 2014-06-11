2e12f5c18c0a7454f7ed7f381d09f77490c1e2b0
runtropolis <matt3r@gmail.com>
2014-06-11 16:56:36 -0400

    post form not working yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611200222) do
+ActiveRecord::Schema.define(version: 20140611203232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: true do |t|
+    t.integer  "post_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title",      null: false
