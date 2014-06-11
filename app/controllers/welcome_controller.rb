b46ebec06e65347bf3d95af771e2f30678bd1077
runtropolis <matt3r@gmail.com>
2014-06-11 16:12:04 -0400

    updated routes and added blogs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,10 @@
 class WelcomeController < ApplicationController
+  before_action :find_blog, only: [:show, :edit, :update, :destroy]
 
+
+  private
+
+    def find_blog
+      @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :blogs do
+  resources :blogs, only: [:show] do
     resources :posts
   end
 
