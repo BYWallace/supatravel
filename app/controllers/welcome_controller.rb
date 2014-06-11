c389058b0535bf6e55cb6ae82ae4e6a551faea68
GS888 <gsnydermail@gmail.com>
2014-06-11 18:24:41 -0400

    created a seed post in the seed file. created a method in welcome controller to furnish data to map. created a partial to add info to the map pin.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,20 @@
 class WelcomeController < ApplicationController
   before_action :find_blog, only: [:show, :edit, :update, :destroy]
 
+    def index
+      @posts = Post.all
+      @hash = Gmaps4rails.build_markers(@posts) do |post, marker|
+        @post = post
+        marker.lat post.latitude
+        marker.lng post.longitude
+        marker.infowindow render_to_string(:partial => "/welcome/info_window", :locals => { :object => post}).gsub(/\n/, '')
+        marker.picture({
+                    :url => "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+                    :width   => 36,
+                    :height  => 36
+        })
+      end
+    end
 
   private
 
