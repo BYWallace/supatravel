ad6c13a9adead10cd086d449dd4937da1f6861c9
ef718 <elfang16@gmail.com>
2014-06-12 16:28:30 -0400

    Added photo index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-    resources :photos, only: [:new, :create, :show, :destroy]
+    resources :photos, only: [:index, :new, :create, :show, :destroy]
     resources :posts
   end
 
