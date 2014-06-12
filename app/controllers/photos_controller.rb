f22347fc837cb667ecb9cbc96f4f43d3350cc941
ef718 <elfang16@gmail.com>
2014-06-12 10:12:21 -0400

    Adding photo model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,11 @@
 class PhotosController < ApplicationController
+  before_action(:load_user, only: [:index, :new, :create, :show, :edit, :update, :destroy] )
+  before_action :require_current_user, except: [:index, :show, :search]
+  before_action(:load_photo, { only: [:edit, :update, :show, :destroy] })
 
   def index
-    @photos = Photo.all
+    page_user = User.find_by(id: params[:user_id])
+    @photos = page_user.photos.reverse
   end
 
   def new
@@ -9,5 +13,26 @@ class PhotosController < ApplicationController
 
   def create
   end
+
+
+  private
+
+    def load_user
+      return @user = User.find_by(id: params[:user_id])
+    end
+
+    def load_photo
+    return @photo = Photo.find_by(id: params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:photo_image, :user_id)
+    end
+
+    def require_current_user
+      if !current_user?(@user)
+        redirect_to root_path
+      end
+    end
 
 end
