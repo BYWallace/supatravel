29814675c06329cac90d3c6c377573389952c32f
runtropolis <matt3r@gmail.com>
2014-06-12 16:36:10 -0400

    style fix

--- app/assets/javascripts/prettythings.js
+++ app/assets/javascripts/prettythings.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-  $('.main').blurjs({
-    source: 'body',
-    radius: 7,
-    overlay: 'rgba(173, 216, 230, .2)',
-    cache: true
-  });
-});
+// $(document).ready(function(){
+//   $('.main').blurjs({
+//     source: 'body',
+//     radius: 7,
+//     overlay: 'rgba(173, 216, 230, .2)',
+//     cache: true
+//   });
+// });
