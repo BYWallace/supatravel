45b0c9b66622409da78c75d864f0904979525222
Inhak Kim <inhakki@gmail.com>
2014-06-12 14:34:16 -0400

    Heroku compatible

--- app/assets/javascripts/prettythings.js
+++ app/assets/javascripts/prettythings.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.main').blurjs({
     source: 'body',
     radius: 10,
-    overlay: 'rgba(173, 216, 230, .2)'
+    overlay: 'rgba(173, 216, 230, .2)',
+    cache: true
   });
 });
