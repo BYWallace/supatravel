b1277a989f20554b53458a110837fc9045a38e94
Inhak Kim <inhakki@gmail.com>
2014-06-12 10:31:16 -0400

    blur works

--- app/assets/javascripts/prettythings.js
+++ app/assets/javascripts/prettythings.js
@@ -1,7 +1,7 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
   $('.main').blurjs({
     source: 'body',
-    radius: 40
+    radius: 10,
+    overlay: 'rgba(80, 10, 180, .2)'
   });
 });
-*/
