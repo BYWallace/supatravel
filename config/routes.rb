3076fc1e10bd2c5b7fa12ddd7a1eccf8fc3a5332
runtropolis <matt3r@gmail.com>
2014-06-12 09:27:53 -0400

    updated routes for photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
+    resources :photos, only: [:new, :index, :create, :destroy]
     resources :posts
   end
 
