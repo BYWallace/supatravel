865d10c3a1b295a683aeef67311c2ee64656e35a
BYWallace <bwallace90@gmail.com>
2014-06-12 10:24:56 -0400

    Tweaked photos schema and added attachment relations table between post and photos

---
+++ db/migrate/20140612142120_create_attachments.rb
@@ -0,0 +1,10 @@
+class CreateAttachments < ActiveRecord::Migration
+  def change
+    create_table :attachments do |t|
+      t.references :post
+      t.references :photo
+
+      t.timestamps
+    end
+  end
+end
